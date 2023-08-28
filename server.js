8e5187ebaf200e608b6a95e3eff5e778775f9d84
Nouman Khan <noumank0han@gmail.com>
2023-08-28 20:18:41 +0500

    init commit

---
+++ server.js
@@ -0,0 +1,23 @@
+import express from 'express';
+import colors from 'colors';
+import dotenv from 'dotenv';
+
+
+//configure env
+dotenv.config();
+
+//rest object
+const app = express();
+
+//rest api
+app.get('/', (req, res) => {
+    res.send('<h1> Welcome to E-commerce App </h1>');
+})
+
+//Port
+const PORT = process.env.PORT || 8080;
+
+//run listen
+app.listen(PORT, () => {
+    console.log(`Server running on ${process.env.DEV_MODE} server on PORT ${PORT}`.bgCyan.white.bold)
+})
